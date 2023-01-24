df6253f9f423953a5852ddf3276656e6090e5b86
Simon <s.eliseev@urbamatica.com>
2023-01-24 23:35:56 +0300

    Modify admin dashboard and models

--- src/tasks/admin.py
+++ src/tasks/admin.py
@@ -1,8 +1,28 @@
 from django.contrib import admin
+
 from tasks import models
+
+
 # Register your models here.
+
+
+class CommentInline(admin.TabularInline):
+    model = models.Comment
+    can_delete = False
+    readonly_fields = ['owner', 'text', 'task']
+    extra = 0
 
 
 @admin.register(models.Task)
 class TaskAdminModel(admin.ModelAdmin):
+    inlines = [CommentInline]
+
+
+@admin.register(models.Comment)
+class CommentAdminModel(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Status)
+class StatusAdminModel(admin.ModelAdmin):
     pass
--- src/tasks/models.py
+++ src/tasks/models.py
@@ -1,18 +1,35 @@
-from django.db import models
 from django.contrib.auth import models as user_model
+from django.db import models
+
 from core import models as core_models
+
+
 # Create your models here.
 
 class Status(core_models.BaseModel):
     title = models.CharField(max_length=128, blank=True)
     color = models.CharField(max_length=128, blank=True)
 
+    class Meta:
+        verbose_name_plural = 'Statuses'
+
+    def __str__(self):
+        return f'{self.title}'
+
+
 class Task(core_models.BaseModel):
     title = models.CharField(max_length=128, blank=True)
     text = models.CharField(max_length=255, blank=True)
     status = models.ForeignKey(Status, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f'{self.title} | {self.status}'
+
+
 class Comment(core_models.BaseModel):
     owner = models.ForeignKey(user_model.User, on_delete=models.CASCADE)
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
     text = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return f'{self.owner.username} | {self.created}'
