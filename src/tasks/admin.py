5431670d050f9d76a8cb0a04349143027d85d27f
Simon <s.eliseev@urbamatica.com>
2023-01-24 23:15:40 +0300

    Base model, init Status, Task and Comment models

--- src/tasks/admin.py
+++ src/tasks/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from tasks import models
 # Register your models here.
+
+
+@admin.register(models.Task)
+class TaskAdminModel(admin.ModelAdmin):
+    pass
--- src/tasks/models.py
+++ src/tasks/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.contrib.auth import models as user_model
+from core import models as core_models
 # Create your models here.
+
+class Status(core_models.BaseModel):
+    title = models.CharField(max_length=128, blank=True)
+    color = models.CharField(max_length=128, blank=True)
+
+class Task(core_models.BaseModel):
+    title = models.CharField(max_length=128, blank=True)
+    text = models.CharField(max_length=255, blank=True)
+    status = models.ForeignKey(Status, on_delete=models.CASCADE)
+
+class Comment(core_models.BaseModel):
+    owner = models.ForeignKey(user_model.User, on_delete=models.CASCADE)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    text = models.CharField(max_length=255, blank=True)
